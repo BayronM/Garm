83d5183ed2fb6c23933aa0af19fa14dad4410e6c
Bayron Monsalvez <bayronmonsalvez@gmail.com>
2019-11-03 23:26:30 -0300

    Se incluye Electron a La Aplicacion

--- index.js
+++ index.js
@@ -1,30 +1,78 @@
+const { app, BrowserWindow } = require('electron')
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 
 const path = require('path');
 
-//webpack
+//Configuracion de Webpack(para enviar los archivos necesarios al cliente)
 const webpack = require('webpack');
 const webpackMiddleware = require('webpack-dev-middleware');
 const config = require('./webpack.config')
 
-const app = express();
-const server = http.createServer(app);
+const application = express();
+const server = http.createServer(application);
+
+//Configuracion de Socket.io para la actulizacion en tiempo real de los clientes
 const io = socketIo(server);
-
-
-
-app.use(webpackMiddleware(webpack(config)));
-
-app.use(express.static(path.join(__dirname, 'public')));
-server.listen(3000, () => console.log('server on port 3000'));
 
 io.on('connection', socket => {
     console.log('socket connected: ', socket.id);
 });
 
+application.use(webpackMiddleware(webpack(config)));
 
+application.use(express.static(path.join(__dirname, 'public')));
+server.listen(3000, () => console.log('server on port 3000'));
+
+let win
+
+function createWindow() {
+    // Crea la ventana del navegador.
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+
+    // Carga la aplicacion desde localhost (se podria dejar con un puerto variable)
+    win.loadURL('http://localhost:3000')
+
+    // Abre las herramientas de desarrollo (DevTools).
+    win.webContents.openDevTools()
+
+    // Emitido cuando la ventana es cerrada.
+    win.on('closed', () => {
+        // Elimina la referencia al objeto window, normalmente  guardarías las ventanas
+        // en un vector si tu aplicación soporta múltiples ventanas, este es el momento
+        // en el que deberías borrar el elemento correspondiente.
+        win = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    // En macOS es común para las aplicaciones y sus barras de menú
+    // que estén activas hasta que el usuario salga explicitamente con Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // En macOS es común volver a crear una ventana en la aplicación cuando el
+    // icono del dock es clicado y no hay otras ventanas abiertas.
+    if (win === null) {
+        createWindow()
+    }
+})
+
+
+
+//Conexion de Puerto serial
 const SerialPort = require('serialport');
 const ReadLine = SerialPort.parsers.Readline;
 
@@ -45,3 +93,4 @@ parser.on('data', function (data) {
 
 parser.on('error', (err) => console.log(err));
 port.on('error', (err) => console.log(err));
+
